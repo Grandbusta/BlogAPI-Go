90c1fc425f05165b392bf3a04f814eab8d54e4c0
grandbusta <bustajay30@gmail.com>
2021-05-02 05:05:29 +0100

    db config modified

--- db/db_connect.go
+++ db/db_connect.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -13,7 +14,8 @@ func DbConfig() (*sql.DB, error) {
 	DB_USER := os.Getenv("DB_USER")
 	DB_HOST := os.Getenv("DB_HOST")
 	DB_PORT := os.Getenv("DB_PORT")
-	db, err := sql.Open("mysql", DB_USER+":"+DB_PASS+"@tcp("+DB_HOST+":"+DB_PORT+")/"+DB_NAME)
+	DB_URL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
+	db, err := sql.Open("mysql", DB_URL)
 	if err != nil {
 		return nil, err
 	}
